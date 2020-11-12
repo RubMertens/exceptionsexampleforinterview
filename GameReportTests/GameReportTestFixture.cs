0ae7ac3008e763543a96bc5a45d226a3c3eca3ef
BennyM <bennym@me.com>
2020-11-12 13:57:57 +0100

    Added a new requirement

--- GameReportTests/GameReportTestFixture.cs
+++ GameReportTests/GameReportTestFixture.cs
@@ -8,6 +8,10 @@ namespace GameReportTests
     public class GameReportTestFixture
     {
         private SqliteConnection _connection;
+        private Player _ruben;
+        private Player _benny;
+        private GameBuilder _gameBuilder;
+
 
         [SetUp]
         public void Setup()
@@ -15,6 +19,16 @@ namespace GameReportTests
             _connection = new SqliteConnection("Data Source=:memory:");
             _connection.Open();
             new GamesDatabaseFactory(_connection).GenerateDatabase();
+            _ruben = new PlayerBuilder()
+               .WithId(1)
+               .WithName("Ruben")
+               .Build(_connection);
+            _benny = new PlayerBuilder()
+                .WithId(2)
+                .WithName("Benny")
+                .Build(_connection);
+            _gameBuilder = new GameBuilder()
+               .WithId(42);
         }
 
         [TearDown]
@@ -24,28 +38,72 @@ namespace GameReportTests
         }
 
         [Test]
+        public void CreateReportForGame_NoPlayers_OnlyTitleShown()
+        {
+            _gameBuilder
+                .Build(_connection);
+
+            var reportGenerator = new ReportGenerator(new ScoreRepository(_connection));
+
+            var output = reportGenerator.CreateReportForGame(42);
+
+            Assert.AreEqual(
+                "Score for game 42" + Environment.NewLine, output);
+        }
+
+        [Test]
+        public void CreateReportForGame_WithScores_CalculatesCorrectTotal()
+        {
+            _gameBuilder
+                .WithParticipant(_ruben)
+                .WithPointFor(_ruben)
+                .WithPointFor(_ruben)
+                .Build(_connection);
+
+            var reportGenerator = new ReportGenerator(new ScoreRepository(_connection));
+
+            var output = reportGenerator.CreateReportForGame(42);
+
+            Assert.AreEqual(
+                "Score for game 42" + Environment.NewLine +
+                "Player: Ruben Score: 2" + Environment.NewLine, output);
+        }
+
+        [Test]
+        public void CreateReportForGame_MultiplePlayersWithPoints_AssignsScoreToEachPlayer()
+        {
+            _gameBuilder
+               .WithParticipant(_ruben)
+               .WithParticipant(_benny)
+               .WithPointFor(_ruben)
+               .WithPointFor(_benny)
+               .WithPointFor(_ruben)
+               .Build(_connection);
+
+            var reportGenerator = new ReportGenerator(new ScoreRepository(_connection));
+
+            var output = reportGenerator.CreateReportForGame(42);
+
+            Assert.AreEqual(
+            "Score for game 42" + Environment.NewLine +
+            "Player: Ruben Score: 2" + Environment.NewLine +
+            "Player: Benny Score: 1" + Environment.NewLine, output);
+        }
+
+        [Test]
         public void CreateReportForGame_PlayerWithoutScore_IsIncludedInReport()
         {
-            var ruben = new PlayerBuilder()
-                .WithId(1)
-                .WithName("Ruben")
-                .Build(_connection);
-            var benny = new PlayerBuilder()
-                .WithId(2)
-                .WithName("Benny")
-                .Build(_connection);
             var hanjo = new PlayerBuilder()
                 .WithId(3)
                 .WithName("Hanjo")
                 .Build(_connection);
-            new GameBuilder()
-                .WithId(42)
-                .WithParticipant(ruben)
-                .WithParticipant(benny)
+            _gameBuilder
+                .WithParticipant(_ruben)
+                .WithParticipant(_benny)
                 .WithParticipant(hanjo)
-                .WithPointFor(ruben)
-                .WithPointFor(benny)
-                .WithPointFor(ruben)
+                .WithPointFor(_ruben)
+                .WithPointFor(_benny)
+                .WithPointFor(_ruben)
                 .Build(_connection);
 
             var reportGenerator = new ReportGenerator(new ScoreRepository(_connection));
@@ -57,6 +115,30 @@ namespace GameReportTests
             "Player: Ruben Score: 2" + Environment.NewLine +
             "Player: Benny Score: 1" + Environment.NewLine +
             "Player: Hanjo Score: 0" + Environment.NewLine, output);
+        }
+
+        [Test]
+        public void CreateReportForGame_PlayersWithIdenticalName_CorrectlyAssignScore()
+        {
+            var otherBenny = new PlayerBuilder()
+                .WithId(3)
+                .WithName("Benny")
+                .Build(_connection);
+            _gameBuilder
+                .WithParticipant(_benny)
+                .WithParticipant(otherBenny)
+                .WithPointFor(_benny)
+                .WithPointFor(otherBenny)
+                .Build(_connection);
+
+            var reportGenerator = new ReportGenerator(new ScoreRepository(_connection));
+
+            var output = reportGenerator.CreateReportForGame(42);
+
+            Assert.AreEqual(
+            "Score for game 42" + Environment.NewLine +
+            "Player: Benny (2) Score: 2" + Environment.NewLine +
+            "Player: Benny (3) Score: 1" + Environment.NewLine, output);
         }
     }
 }
--- README.md
+++ README.md
@@ -20,9 +20,11 @@ System.NullReferenceException : Object reference not set to an instance of an ob
 ## GameReport
 
 ### Requirements
-Run the tests of the GameReportTests project. We have a failing requirement. Try to locate the problem and get the test green.
+Run the tests of the GameReportTests project. We have a couple of failing requirements. Try to locate the problem of the `CreateReportForGame_PlayerWithoutScore_IsIncludedInReport` test and get it green.
 
 ### Code review
 - The `ScoreRepository` class has a vulnerability. Which one and how can we fix it.
 - The `GameBuilder` class uses a `HashSet` and a `List`, why? There is also a `participantMap` variable, how does it work? Should we improve the `Player` class to enforce our design?
 
+### Identical names
+A new requirement has been introduced: players can have identical names. If this is the case the score should still be calculated correctly and their technical id needs to be included in the report. The test `CreateReportForGame_PlayersWithIdenticalName_CorrectlyAssignScore` expresses this new requirement.
